bb203d4bacff7fb2a24e0a5ceda46981533fe9e8
annienf17 <a.ulanskaxxi@gmail.com>
2024-09-05 17:22:53 +0100

    env secure API key

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -13,8 +13,7 @@ function HomePage() {
           {
             headers: {
               accept: "application/json",
-              Authorization:
-                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NmY0MTJhMzNhNTUyYjhkMzAzOGFkYTlmOWIzOWE0OCIsIm5iZiI6MTcyNTU0NzE4NC43OTUzMzgsInN1YiI6IjY1MGFmNjRkYWVkZTU5MWFiMjYwZWZmYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FSZ-2PklulCMgZm5zzagTG6hgAnwmR-S659Vj6MzSz8",
+              Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
             },
           }
         );
