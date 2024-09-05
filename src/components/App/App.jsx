7cb967dde6e4172a0b87864781dc7086b7289ad3
annienf17 <a.ulanskaxxi@gmail.com>
2024-09-05 14:38:53 +0100

    dynamically generates a list of movie elements

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,27 +1,82 @@
+import { useEffect, useState } from "react";
 import css from "./App.module.css";
 
 function App() {
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NmY0MTJhMzNhNTUyYjhkMzAzOGFkYTlmOWIzOWE0OCIsIm5iZiI6MTcyNTQ3NjAxNy41OTE1ODIsInN1YiI6IjY1MGFmNjRkYWVkZTU5MWFiMjYwZWZmYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qVQ1HMz4sng209RtHKKDDWadxNRnMDhRN8lsOfcRoc4",
-    },
-  };
+  const [movies, setMovies] = useState([]);
+  const [config, setConfig] = useState(null);
 
-  fetch(
-    "https://api.themoviedb.org/3/trending/movie/day?language=en-US",
-    options
-  )
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
+  useEffect(() => {
+    const fetchConfig = async () => {
+      const configOptions = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NmY0MTJhMzNhNTUyYjhkMzAzOGFkYTlmOWIzOWE0OCIsIm5iZiI6MTcyNTQ3NjAxNy41OTE1ODIsInN1YiI6IjY1MGFmNjRkYWVkZTU5MWFiMjYwZWZmYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qVQ1HMz4sng209RtHKKDDWadxNRnMDhRN8lsOfcRoc4",
+        },
+      };
+
+      try {
+        const response = await fetch(
+          "https://api.themoviedb.org/3/configuration",
+          configOptions
+        );
+        const data = await response.json();
+        setConfig(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    const fetchMovies = async () => {
+      const movieOptions = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NmY0MTJhMzNhNTUyYjhkMzAzOGFkYTlmOWIzOWE0OCIsIm5iZiI6MTcyNTQ3NjAxNy41OTE1ODIsInN1YiI6IjY1MGFmNjRkYWVkZTU5MWFiMjYwZWZmYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qVQ1HMz4sng209RtHKKDDWadxNRnMDhRN8lsOfcRoc4",
+        },
+      };
+
+      try {
+        const response = await fetch(
+          "https://api.themoviedb.org/3/trending/movie/day?language=en-US",
+          movieOptions
+        );
+        const data = await response.json();
+        setMovies(data.results);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchConfig();
+    fetchMovies();
+  }, []);
+
+  const getImageUrl = (path) => {
+    if (!config) return "";
+    const baseUrl = config.images.secure_base_url;
+    const size = config.images.poster_sizes[3]; // w500 size
+    return `${baseUrl}${size}${path}`;
+  };
 
   return (
     <>
       <div className={css.body}>
         <h1>Movie Library</h1>
+        <div className={css.movies}>
+          {movies.map((movie) => (
+            <div key={movie.id} className={css.movie}>
+              <img
+                src={getImageUrl(movie.poster_path)}
+                alt={movie.title}
+                className={css.poster}
+              />
+              <h2>{movie.title}</h2>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
