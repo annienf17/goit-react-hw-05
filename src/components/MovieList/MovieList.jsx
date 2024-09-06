4ee58df8430c564f06c59f64531f3653ddf40baa
annienf17 <a.ulanskaxxi@gmail.com>
2024-09-06 14:04:21 +0100

    MovieList wytyczne

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,11 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function MovieList({ movies }) {
+  const location = useLocation(); // Użycie hooka useLocation
+
   return (
     <ul>
       {movies.map((movie) => (
         <li key={movie.id}>
-          <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+          <Link
+            to={`/movies/${movie.id}`}
+            state={{ from: location }} // Dodanie właściwości state z wartością location
+          >
+            {movie.title}
+          </Link>
         </li>
       ))}
     </ul>
